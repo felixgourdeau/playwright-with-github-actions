bc57adfa884ec6f9a93fec342ebffb9b04821193
Felix Gourdeau <felix.gourdeau@gmail.com>
2024-10-22 12:11:37 -0400

    no message

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,14 +1,26 @@
 import { defineConfig, devices } from '@playwright/test';
 
+console.log(devices['Desktop Safari'])
+
 export default defineConfig({
   projects: [
     {
       name: 'webkit',
       use: { 
         ...devices['Desktop Safari'],
-        isMobile: true,
-        hasTouch: true,
-        deviceScaleFactor: 3,
+        "userAgent": "Mozilla/5.0 (iPhone; CPU iPhone OS 15_0 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/18.0 Mobile/15E148 Safari/604.1",
+        "screen": {
+          "width": 390,
+          "height": 844
+        },
+        "viewport": {
+          "width": 390,
+          "height": 664
+        },
+        "deviceScaleFactor": 3,
+        "isMobile": true,
+        "hasTouch": true,
+        "defaultBrowserType": "webkit"
       },
     },
   ],
