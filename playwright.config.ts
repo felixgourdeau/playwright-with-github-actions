74ec912063a1ff8a6ebe64f07a25afbc71c70953
Felix Gourdeau <felix.gourdeau@gmail.com>
2024-10-22 12:22:53 -0400

    no message

--- playwright.config.ts
+++ playwright.config.ts
@@ -10,15 +10,15 @@ export default defineConfig({
       name: 'webkit',
       use: { 
         ...devices['Desktop Safari'],
-        "userAgent": "Mozilla/5.0 (iPhone; CPU iPhone OS 15_0 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/18.0 Mobile/15E148 Safari/604.1",
-        "screen": {
-          "width": 390,
-          "height": 844
-        },
-        "viewport": {
-          "width": 390,
-          "height": 664
-        },
+        //"userAgent": "Mozilla/5.0 (iPhone; CPU iPhone OS 15_0 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/18.0 Mobile/15E148 Safari/604.1",
+        //"screen": {
+        //  "width": 390,
+        //  "height": 844
+        //},
+        //"viewport": {
+        //  "width": 390,
+        //  "height": 664
+        //},
         "deviceScaleFactor": 3,
         "isMobile": true,
         "hasTouch": true,
